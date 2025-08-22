ee9364d7a4a16c4486814ea8e68bb7dd8e01ff2f
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:23:05 -0500

    cambios en tipos basicos

--- TiposBasicos/tiposbasicos/main.cpp
+++ TiposBasicos/tiposbasicos/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-
 using namespace std;
+
+
 
 int main()
 {
-    int val;
-    cin >> val;
-    cout << "El valor de la variable es: " << val << endl;
+    int val1 = 10;
+    int val2 = 20;
+    int sum = val1 +val2;
+    int mult = val1*val2;
+    float div = val1 / val2;
+
+    cout << "el primer valor es: " << val1<<endl;
+    cout << "El segundo valor es: " << val2 << endl;
+    cout << "El resultado de la suma es: " << sum << endl;
+    cout << "El resultado de la multiplicacion es: " << mult << endl;
+    cout << "El resultado de la division es: " << div << endl;
+
 
     return 0;
 }
