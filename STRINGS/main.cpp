f17dd040ac4e6f4700f8887510433a65c812f559
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2025-09-17 08:13:39 -0500

    cambios realizados

--- STRINGS/main.cpp
+++ STRINGS/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 /*
 "" ---> Para cadenas
@@ -13,13 +13,46 @@ using namespace std;
 int main()
 {
 
-    string cad = "Ciencia de la Computacion" ;
-    cout << cad.at(5) << endl; /// ---> imprime caracter en dicha posición [0, n]
-
+    string cad = "CIENCIA DE LA COMPUTACION" ;
+    ///cout << cad.at(5) << endl; /// ---> imprime caracter en dicha posición [0, n]
     int i= 0;
+    int cont {0};
+    char g;
+    ///cout << "Letra para buscar: " << endl;
+    ///cin >> g;
+    /*
     while (cad.at(i) != '\0'){
         i++;
     }
-    cout << i << endl;
+    */
+    while (i < cad.length()){
+        if (cad[i] == g){
+            cont += 1;
+        }
+        i++;
+    }
+
+    /// VERFICAR SI LA CADENA ES PALIDROMO
+
+    string cadena = "reconocer";
+
+    for (int i= 0; i < cadena.length(); i++){
+        for (int j= 0; j > 0; j--){
+            if (cadena[i] == cadena[j]){
+                cout << "La cadena es paindroma " << endl;
+            }else{
+                cout << "No es palindromo " << endl;
+            }
+        }
+    }
+
+
+
+    ///cout << "La letra es: " << cad.at(5) << endl;
+    ///cout << "Se repite: " << cont << " veces" << endl;
     return 0;
 }
+
+
+
+
